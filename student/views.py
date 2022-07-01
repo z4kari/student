a581cf8b022bf387bcf98ac45dbb69aa31766e55
z4kari <mhdzakari2001@gmail.com>
2022-07-01 19:57:01 +0530

    level1

---
+++ student/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def login(request):
+    return render(request,'student/login.html')
+
+def home(request):
+    return render(request,'student/home.html')
